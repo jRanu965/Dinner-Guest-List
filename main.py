b14d69c38675d78a88c12f8f9dda38c1c836b548
jRanu965 <jasnoorr965@deltalearns.ca>
2026-03-05 12:28:56 -0800

    added some things
    add guest started on guest names

--- main.py
+++ main.py
@@ -10,4 +10,17 @@ Dinner Guest list
 guest_names = []
 guest_meals = []
 
-def
+def add_guest():
+    """Add a guest to the list."""
+    name = input("Enter guest name: ").title().strip()
+
+    # Check for empty name
+    if name == "":
+        print("Name cannot be empty.")
+        return
+
+    # Check for duplicate names
+    if name in guest_names:
+        print("That guest is already on the list.")
+        return
+    guest_names 
