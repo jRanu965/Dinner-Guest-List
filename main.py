a569c13c0ac84215ed852cc5bda7bc637e08e129
jRanu965 <jasnoorr965@deltalearns.ca>
2026-03-05 12:20:29 -0800

    jjust started
    yes

---
+++ main.py
@@ -0,0 +1,13 @@
+"""
+Dinner Guest list
+- Add guest.
+- Modify guest.
+- Remove guest.
+- Sort guests.
+- Show number of guests.
+- Show invitations.
+"""
+guest_names = []
+guest_meals = []
+
+def
